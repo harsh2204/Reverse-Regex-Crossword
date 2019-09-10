5ddbaba3e4fa0e6b28d50046dbe6f810a6034524
Harsh Gupta <harshgupta2204@live.com>
2019-09-10 00:03:31 -0400

    Completed the grid module. Started some simple pattern generation

---
+++ grid.py
@@ -0,0 +1,95 @@
+import re
+import os
+import random
+from math import sqrt
+import numpy as np
+
+
+def is_square(n):
+    return sqrt(n).is_integer()
+
+def print_empty_grid(x, y):    
+    print("+"+"--+"*x)
+    for _ in range(y):
+        for _ in range(x):
+            print("|  ", end='')
+        print("|")                        
+        print("+"+"--+"*x)    
+
+def print_grid(l: list):    
+    x = len(l[0])
+    print("+"+"--+"*x)
+    for row in l:
+        print("|"+" |".join(list(row)), end = '')
+        print(" |")                        
+        print("+"+"--+"*x)  
+
+def setup_grid(s:str, use_default=True):
+    n = len(s)
+    if n%2 and not is_square(n):
+        print("String not compatible due to odd length:", n)
+        return
+    
+    
+    combs = []
+    if n <= 4:
+        combs.append(set([n//2, n//2]))
+    for j in range(2,n//2):
+        print(j)
+        tup = set([j, n//j])
+        if n%j==0 and tup not in combs:
+            combs.append(tup)
+    
+    print(combs)
+    if len(combs) == 0:
+        print("Could not find any combinations")
+        return
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    selection = list(combs[-1])
+    if not use_default:
+        for c, v in enumerate(combs, 1):
+            if len(v) == 1:
+                v = list(v)*2
+            x, y = v
+            print(f'[{c}] {x} x {y} grid')
+            rows = []
+            if x < y:
+                rows = re.findall('.{1,'+str(y)+'}', s)        
+            else:
+                rows = re.findall('.{1,'+str(x)+'}', s)        
+            print_grid(rows)
+
+        resp = input('Please select a configuration for the crossword (the number inside []): ')
+
+        assert resp.isdigit(), "Please enter an integer!"
+        resp = int(resp)
+
+        assert resp<=len(combs), f"Please enter a value in range 1, {str(len(combs))}"
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        selection = list(combs[resp-1])    
+
+    rows = []
+    if len(selection) > 1:
+        choices = [
+            re.findall('.{1,'+str(selection[0])+'}', s),
+            re.findall('.{1,'+str(selection[1])+'}', s)
+        ]
+        if not use_default:
+            selection = [str(x) for x in selection]
+            print("\n[0]", " x ".join(selection))
+            print_grid(choices[0])
+            selection.reverse()
+            print("\n[1]", " x ".join(selection))
+            print_grid(choices[1])
+            ori = input("Please select an orientation for the grid: ")        
+        else:
+            ori = 0
+        rows = choices[int(ori)]        
+
+    else:
+        rows = re.findall('.{1,'+str(selection[0])+'}', s)
+        
+    rows = [list(x) for x in rows]
+    return np.array(rows)
--- regex.py
+++ regex.py
@@ -1,92 +1,35 @@
 import re
-import os
-import random
+import numpy as np
+
+from grid import setup_grid
 # pat = 'HE|LL|O+'
 # pat = '[^SPEAK]+'
-
-# try :
-#     a = re.compile(pat)
-#     print("regex compiled successfuly.")
-# except:
-#     print("regex compilation failed.")
-
-
-
-def print_empty_grid(x, y):    
-    print("+"+"--+"*x)
-    for _ in range(y):
-        for _ in range(x):
-            print("|  ", end='')
-        print("|")                        
-        print("+"+"--+"*x)    
-
-def print_grid(l: list):    
-    x = len(l[0])
-    print("+"+"--+"*x)
-    for row in l:
-        print("|"+" |".join(list(row)), end = '')
-        print(" |")                        
-        print("+"+"--+"*x)  
-
-def draw_grid(s:str):
-    n = len(s)
-    if n%2:
-        print("String not compatible due to odd length:", n)
-        return
-    
-    
-    combs = []
-    if len(s) <= 4:
-        combs.append(set([len(s)//2, len(s)//2]))
-    for j in range(2,n//2):
-        print(j)
-        tup = set([j, n//j])
-        if n%j==0 and tup not in combs:
-            combs.append(tup)
-    
-    print(combs)
-    if len(combs) == 0:
-        print("Could not find any combinations")
-        return
-
-    os.system('cls' if os.name == 'nt' else 'clear')
-    for c, v in enumerate(combs, 1):
-        x, y = v if len(v) == 2 else list(v)[0], list(v)[0]
-        print(f'[{c}] {x} x {y} grid')
-        rows = []
-        if x < y:
-            rows = re.findall('.{1,'+str(y)+'}', s)        
-        else:
-            rows = re.findall('.{1,'+str(x)+'}', s)        
-        print_grid(rows)
-
-    resp = input('Please select a configuration for the crossword (the number inside []): ')
-
-    assert resp.isdigit(), "Please enter an integer!"
-    resp = int(resp)
-
-    assert resp<=len(combs), f"Please enter a value in range 1, {str(len(combs))}"
-
-    os.system('cls' if os.name == 'nt' else 'clear')
-    selection = list(combs[resp-1])
-    if len(selection) > 1:
-        choices = [
-            re.findall('.{1,'+str(selection[0])+'}', s),
-            re.findall('.{1,'+str(selection[1])+'}', s)
-        ]
-        selection = [str(x) for x in selection]
-        print("\n[0]", " x ".join(selection))
-        print_grid(choices[0])
-        selection.reverse()
-        print("\n[1]", " x ".join(selection))
-        print_grid(choices[1])
-        ori = input("Please select an orientation for the grid: ")
-        x, y = choices[int(ori)]
-        rows = re.findall('.{1,'+str(x)+'}', s)
-    # print(rows)    
 
 
 # random_words = open('accepted_words').read().split("\n")
 # sel = random.choice(random_words)
 # print("@"+sel+"@")
-draw_grid("four")
+def check_pattern(pat):
+    try :
+        a = re.compile(pat)
+        # print("regex compiled successfuly.")
+        return True
+    except:
+        return False
+
+def generate_pattern(l):
+    print(l)
+    letters = np.unique(l)
+    print("letters")
+    simple = "("+"|".join(letters) + ")+"
+    print("simple regex: '" + simple + "' validity_test_pass: ", check_pattern(simple))
+    # print(letters)
+
+
+
+matrix = setup_grid("TwelveLetter".upper())
+
+print(matrix)
+
+print("---Generating Patterns---")
+generate_pattern(matrix[:, 0])
